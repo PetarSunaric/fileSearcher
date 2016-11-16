96e17a8902d4a824e16bb4633846d38136d376c9
Petar Sunaric <pekisunaric@gmail.com>
2016-11-16 13:10:18 +0100

    inicijalizacija 2

--- src/fileSearcher/FileSearcher.java
+++ src/fileSearcher/FileSearcher.java
@@ -1,5 +1,30 @@
 package fileSearcher;
 
+import java.io.File;
+
 public class FileSearcher {
 
+	public static void getBrojRijeci(File file) {
+
+	}
+
+	public static void getBrojUnikatnihRijeci(File file) {
+
+	}
+
+	public static void getBrojRecenica(File file) {
+
+	}
+
+	public static void getBrojSlova(File file) {
+
+	}
+
+	public static void getBrojKaraktera(File file) {
+
+	}
+
+	public static void getBrojPonavljanjaSlova(File file) {
+
+	}
 }
