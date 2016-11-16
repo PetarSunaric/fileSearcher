568fbb69ddcafa07dea34f8bbc44365acfb2181b
Mensur Hadzic <hadzic.mensur@hotmail.com>
2016-11-16 21:50:06 +0100

    dodana metoda za brojac recenica

--- src/fileSearcher/FileSearcher.java
+++ src/fileSearcher/FileSearcher.java
@@ -1,6 +1,8 @@
 package fileSearcher;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class FileSearcher {
 
@@ -13,7 +15,22 @@ public class FileSearcher {
 	}
 
 	public static void getBrojRecenica(File file) {
-
+		int brojacRecenica = 0;
+		// Provjeramo da li fajl postoji ili ga je potrebno kreirati
+		if (file.exists()) {			
+			try (java.util.Scanner input = new java.util.Scanner(file)) {
+				// Citamo podatke iz fajla
+				while(input.hasNext()) {
+					if (Character.isLetter(input.next().charAt(0)))
+						brojacRecenica++;
+				}
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		} else {
+			System.out.println("Fajl ne postoji");	
+		}
+		// CRATITITITITIT
 	}
 
 	public static void getBrojSlova(File file) {
